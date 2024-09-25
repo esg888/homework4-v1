e7436a705e6a5e1b4ac535ba2e000338e62eb5d4
grou <esg888@yandex.ru>
2024-09-25 17:18:38 +0300

    Кажется, поняла, как засунуть аргументы в аспект, разбираюсь дальше. А вот с валидацией беда...

--- UsersNews/src/main/java/com/example/UsersNews/aop/CheckAspect.java
+++ UsersNews/src/main/java/com/example/UsersNews/aop/CheckAspect.java
@@ -1,11 +1,16 @@
 package com.example.UsersNews.aop;
 
+import com.example.UsersNews.Entity.Item;
+import com.example.UsersNews.Entity.User;
+import com.example.UsersNews.Err.CheckUserException;
 import com.example.UsersNews.Service.ItemsService;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
+
+import java.util.Objects;
 
 @Aspect
 @Component
@@ -23,9 +28,10 @@ public class CheckAspect {
 
 
 
-    @Before(value = "execution(*ItemsService.delete(Integer itemId, Integer ownerId)) && args(itemId, ownerId)")
+    @Before("@annotation(CheckingIt) && args(idItem, ownerId)")
     public void CheckPointcut(JoinPoint joinPoint, Integer idItem, Integer ownerId) {
         itemsService.checkIt(idItem, ownerId);
+              joinPoint.getSignature().getName();
     }
 
 
